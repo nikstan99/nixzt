187f48f405e7b020d36a80ffee1495505f9cf583
Nikola Stanev <nikis@outcon.eu>
2023-06-16 15:42:31 +0300

    setting up tailwind and testing useFetch with Wordpress

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-9xl">
+      nixzt.
+    </h1>
+    <ul class="list-disc list-inside">
+      <li v-for="post in response" :key="post.id">
+        <sapn>{{ post.title.rendered }}</sapn>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: response } = useFetch(
+  "http://nixzt.wpengine.com/wp-json/wp/v2/posts"
+);
+</script>
