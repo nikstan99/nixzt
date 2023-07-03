385735455e7b12224ed7668b4f20875f205aa077
nikstan99 <nikis@outcon.eu>
2023-07-03 22:40:06 +0300

    the input filed component

--- components/UI/Input.vue
+++ components/UI/Input.vue
@@ -1,3 +1,23 @@
 <template>
-  <input type="text" />
+  <div>
+    <label
+      for="input-group-1"
+      class="block mb-2 text-sm font-medium text-gray-900"
+    >
+      Your Email
+    </label>
+    <div class="relative mb-6">
+      <div
+        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+      >
+        <UIIcon class="text-xl" icon="envelope" />
+      </div>
+      <input
+        type="text"
+        id="input-group-1"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+        placeholder="name@flowbite.com"
+      />
+    </div>
+  </div>
 </template>
